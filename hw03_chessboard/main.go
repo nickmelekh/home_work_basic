e55145b5880c41035839289b56ef16d732cb7d9d
nickmelekh <melehna@mail.ru>
2024-03-19 15:03:25 +0300

    draft

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
-	var size uint16
-	fmt.Scanln(&size)
+	var size int
+	var b bytes.Buffer
 
-	fmt.Println("Size: ", size)
+	fmt.Scanf("%d", &size)
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if i%2 == 0 {
+				if j%2 == 0 {
+					b.WriteString("#")
+				} else {
+					b.WriteString(" ")
+				}
+			} else {
+				if j%2 == 0 {
+					b.WriteString(" ")
+				} else {
+					b.WriteString("#")
+				}
+			}
+
+		}
+
+		b.WriteString("\n")
+	}
+
+	fmt.Println(b.String())
 }
